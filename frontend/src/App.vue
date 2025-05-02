4c2f9486b9d1d01676cf575b3e5ad76990828f6c
jennifer090 <chenestrella97@gmail.com>
2025-05-02 13:10:02 +0800

    implement a layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,25 @@
 <template>
-  <div>
-    <router-view />
-    <!-- 用來顯示當前路由的組件 -->
-  </div>
+  <v-app>
+    <v-main class="fill-background">
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 export default {
   name: "App",
+
+  data: () => ({
+    //
+  }),
 };
 </script>
+<style>
+html,
+body,
+#app {
+  height: 100%;
+  margin: 0;
+}
+</style>
