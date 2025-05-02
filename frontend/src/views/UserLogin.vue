001b26c908b06fcb2a3bd5c705298e6b1cd6aa03
jennifer090 <chenestrella97@gmail.com>
2025-05-02 13:10:02 +0800

    implement a layout

--- frontend/src/views/UserLogin.vue
+++ frontend/src/views/UserLogin.vue
@@ -1,20 +1,47 @@
 <template>
-  <div class="login-page" style="text-align: center; margin-top: 100px">
-    <h2>Login Page</h2>
-    <button @click="goToTodo">登入</button>
-  </div>
+  <SideBar>
+    <div class="login-page" style="text-align: center; margin-top: 60px">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/zh/thumb/8/83/Taiwan_Semiconductor_Manufacturing_Co_logo.svg/1200px-Taiwan_Semiconductor_Manufacturing_Co_logo.svg.png"
+        alt="TSMC Logo"
+        style="max-width: 100px; max-height: 100px"
+        class="login-icon"
+      />
+      <h2>Login</h2>
+      <v-text-field
+        clearable
+        label="Account"
+        variant="outlined"
+        class="centered-input"
+      />
+      <v-text-field
+        clearable
+        label="Password"
+        variant="outlined"
+        class="centered-input"
+      />
+
+      <v-btn height:20px variant="tonal" @click="goToTodo"> Submit </v-btn>
+    </div>
+  </SideBar>
 </template>
 
 <script>
 import { useRouter } from "vue-router";
 
+import SideBar from "../components/SideBar.vue";
+
 export default {
-  name: "UserLogin", // 建議用多字名稱
+  name: "UserLogin",
+  components: {
+    SideBar, // ✅ 在這裡註冊才會生效
+  },
+
   setup() {
     const router = useRouter();
 
     function goToTodo() {
-      router.push("/todo");
+      router.push("/home");
     }
 
     return {
@@ -23,3 +50,22 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+h2 {
+  margin: 10px auto;
+  font-size: 30px;
+}
+
+.login-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* 這樣內容（input, button）就會置中 */
+  margin-top: 0px;
+}
+.centered-input {
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+}
+</style>
